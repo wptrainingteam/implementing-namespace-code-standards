a1f9dfc92425ac85a0a75ceae5f7aa05c0227dd6
Troy Chaplin <troy.chaplin@gmail.com>
2025-08-27 07:01:19 -0400

    add: namespacing and autoloading

---
+++ Functions/Register_Blocks.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Advanced_Multi_Block;
+
+use Advanced_Multi_Block\Plugin_Paths;
+
+if ( ! defined( 'ABSPATH' ) ) {
+  exit;
+}
+
+class Register_Blocks {
+  public function __construct() {
+      add_action( 'init', array( $this, 'register_blocks' ) );
+  }
+
+  public function register_blocks() {
+      if ( function_exists( 'wp_register_block_types_from_metadata_collection' ) ) {
+          wp_register_block_types_from_metadata_collection( Plugin_Paths::plugin_path() . 'build/blocks', Plugin_Paths::plugin_path() . '/build/blocks-manifest.php' );
+          return;
+      }
+
+      if ( function_exists( 'wp_register_block_metadata_collection' ) ) {
+          wp_register_block_metadata_collection( Plugin_Paths::plugin_path() . 'build/blocks', Plugin_Paths::plugin_path() . '/build/blocks-manifest.php' );
+      }
+
+      $manifest_data = include Plugin_Paths::plugin_path() . 'build/blocks-manifest.php';
+      foreach ( array_keys( $manifest_data ) as $block_type ) {
+          register_block_type( Plugin_Paths::plugin_path() . "build/blocks/{$block_type}" );
+      }
+  }
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'multi-block/namespacing-coding-standards',
+        'pretty_version' => 'dev-trunk',
+        'version' => 'dev-trunk',
+        'reference' => 'ef79ef6b4dfa513f7541e19e290b2a10a66ed649',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'multi-block/namespacing-coding-standards' => array(
+            'pretty_version' => 'dev-trunk',
+            'version' => 'dev-trunk',
+            'reference' => 'ef79ef6b4dfa513f7541e19e290b2a10a66ed649',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
